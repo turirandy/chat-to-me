0e1c1371d86cc9b28973abc1d608c9196ffcc84b
turirandy <turirandy@gmail.com>
2025-09-29 18:49:14 +0300

    chat to meee

---
+++ chattome.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded"), function () {
+// Join form
+document.addEventListener("DOMContentLoaded"), function () {
+    const signUp = document.getElementById("signup");
+    if (signUp) {
+        signUp.addEventListener("submit", function (event) {
+            event.preventDefault(); // prevent default form submission
+            // Simulate save
+            setTimeout(() => {
+                // Scroll or redirect to top (home)
+                window.location.hash = "", // scrolls to top of page
+                // Alternatively: window.location.href ="index.html";
+                alert("Welcome to the Fam ");
+                 signUp.reset();
+                window.scrollTo({ top: 0, behavior: "smooth" });
+            }, 500);
+        });
+    }  
+}}
